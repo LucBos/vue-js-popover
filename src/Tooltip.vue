bd339a8f09df182780ff3f4854444ec70398cc93
Luc Bos <luc_bos@hotmail.com>
2019-08-09 13:01:03 +0200

    FIX: position fixed on parent

--- src/Tooltip.vue
+++ src/Tooltip.vue
@@ -1,33 +1,34 @@
 <template>
-  <popover name="tooltip"
-           :pointer="pointer"
-           :width="width"
-           :event="event"
-           @show="value = $event.value">
-    {{value}}
-  </popover>
+    <popover
+        name="tooltip"
+        :pointer="pointer"
+        :width="width"
+        :event="event"
+        @show="value = $event.value">
+        {{ value }}
+    </popover>
 </template>
 <script>
-export default {
-  name: 'Tooltip',
-  props: {
-    event : {
-      type: String,
-      default: 'hover'
-    },
-    pointer: {
-      type: Boolean,
-      default: false
-    },
-    width: {
-      type: Number,
-      default: 160
-    }
-  },
-  data () {
-    return {
-      value: ''
-    }
-  }
-}
+    export default {
+        name: 'Tooltip',
+        props: {
+            event : {
+                type: String,
+                default: 'hover'
+            },
+            pointer: {
+                type: Boolean,
+                default: false
+            },
+            width: {
+                type: Number,
+                default: 160
+            }
+        },
+        data () {
+            return {
+                value: ''
+            };
+        }
+    };
 </script>
--- src/index.js
+++ src/index.js
@@ -1,81 +1,81 @@
-import Popover    from './Popover.vue'
-import Tooltip    from './Tooltip.vue'
-import { events } from './bus'
+import Popover    from './Popover.vue';
+import Tooltip    from './Tooltip.vue';
+import { events } from './bus';
 
-const defaultPosition = 'bottom'
+const defaultPosition = 'bottom';
 
 const prepareBinding = ({ arg = '', modifiers = {}, value = {} }) => {
-  let mods = Object.keys(modifiers)
-  let name = typeof value === 'object' && value.name ? value.name : arg
-  let position = mods[0] || value.position || defaultPosition
+  let mods = Object.keys(modifiers);
+  let name = typeof value === 'object' && value.name ? value.name : arg;
+  let position = mods[0] || value.position || defaultPosition;
 
-  return { name, position, value }
-}
+  return { name, position, value };
+};
 
 const addClickEventListener = (target, params) => {
   const click = (srcEvent) => {
-    events.$emit('show:click', { ...params, target, srcEvent })
+    events.$emit('show:click', { ...params, target, srcEvent });
 
     let handler = (srcEvent) => {
-      events.$emit('hide:click', { ...params, target, srcEvent })
-      document.removeEventListener('click', handler)
-    }
+      events.$emit('hide:click', { ...params, target, srcEvent });
+      document.removeEventListener('click', handler);
+    };
 
-    document.addEventListener('click', handler)
-    srcEvent.stopPropagation()
-  }
+    document.addEventListener('click', handler);
+    srcEvent.stopPropagation();
+  };
 
-  target.addEventListener('click', click)
+  target.addEventListener('click', click);
 
   target.$popoverRemoveClickHandlers = () => {
-    target.removeEventListener('click', click)
-  }
-}
+    target.removeEventListener('click', click);
+  };
+};
 
 const addHoverEventListener = (target, params) => {
   const mouseenter = (srcEvent) => {
-    events.$emit('show:hover', { ...params, target, srcEvent })
-  }
+    events.$emit('show:hover', { ...params, target, srcEvent });
+  };
 
   const mouseleave = (srcEvent) => {
-    events.$emit('hide:hover', { ...params, target, srcEvent })
-  }
+    events.$emit('hide:hover', { ...params, target, srcEvent });
+  };
 
-  target.addEventListener('mouseenter', mouseenter)
-  target.addEventListener('mouseleave', mouseleave)
+  target.addEventListener('mouseenter', mouseenter);
+  target.addEventListener('mouseleave', mouseleave);
 
   target.$popoverRemoveHoverHandlers = () => {
-    target.removeEventListener('mouseenter', mouseenter)
-    target.removeEventListener('mouseleave', mouseleave)
-  }
-}
+    target.removeEventListener('mouseenter', mouseenter);
+    target.removeEventListener('mouseleave', mouseleave);
+  };
+};
 
 export default {
   install (Vue, params = {}) {
     document.addEventListener('resize', (event) => {
-      events.$emit('hide', { srcEvent: event })
-    })
+      events.$emit('hide', { srcEvent: event });
+    });
 
-    Vue.component('Popover', Popover)
+    Vue.component('Popover', Popover);
 
     Vue.directive('popover', {
       bind: function (target, binding) {
-        let params = prepareBinding(binding)
+        let params = prepareBinding(binding);
 
-        addClickEventListener(target, params)
-        addHoverEventListener(target, params)
+        addClickEventListener(target, params);
+        addHoverEventListener(target, params);
       },
       unbind: function (target, binding) {
-        target.$popoverRemoveHoverHandlers()
-        target.$popoverRemoveClickHandlers()
+        target.$popoverRemoveHoverHandlers();
+        target.$popoverRemoveClickHandlers();
       }
-    })
+    });
 
     if (params.tooltip) {
       if (params.debug) {
-        console.log('vue-js-popover | tooltip active')
+        console.log('vue-js-popover | tooltip active');
       }
-      Vue.component('Tooltip', Tooltip)
+      Vue.component('Tooltip', Tooltip);
     }
   }
-}
+};
